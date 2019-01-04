d4e8c2208b86add8d1ac020ef2cfd8a56ee816b5
xudafeng <xudafeng@126.com>
2019-01-05 01:47:59 +0800

    chore: update config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const macacaEcosystem = require('macaca-ecosystem');
+
 module.exports = {
   dest: 'docs',
   base: '/',
@@ -82,42 +84,7 @@ module.exports = {
             text: 'Guide',
             link: '/guide/'
           },
-          {
-            text: 'Ecosystem',
-            items: [
-              {
-                text: 'Web Page Testing',
-                items: [
-                  {
-                    text: 'UITest',
-                    link: 'https://macacajs.github.io/uitest'
-                  },
-                  {
-                    text: 'Torchjs',
-                    link: 'https://macacajs.github.io/torchjs'
-                  }
-                ]
-              },
-              {
-                text: 'Mock',
-                items: [
-                  {
-                    text: 'DataHub',
-                    link: 'https://macacajs.github.io/macaca-datahub'
-                  }
-                ]
-              },
-              {
-                text: 'UI Crawler',
-                items: [
-                  {
-                    text: 'NoSmoke',
-                    link: 'https://macacajs.github.io/NoSmoke'
-                  }
-                ]
-              }
-            ]
-          }
+          macacaEcosystem.en,
         ],
         sidebar: {
           '/guide/': genSidebarConfig(),
@@ -139,42 +106,7 @@ module.exports = {
             text: '指南',
             link: '/zh/guide/'
           },
-          {
-            text: '生态成员',
-            items: [
-              {
-                text: 'Web 测试框架',
-                items: [
-                  {
-                    text: 'UITest',
-                    link: 'https://macacajs.github.io/uitest/zh/'
-                  },
-                  {
-                    text: 'Torchjs',
-                    link: 'https://macacajs.github.io/torchjs/zh/'
-                  }
-                ]
-              },
-              {
-                text: 'Mock 方案',
-                items: [
-                  {
-                    text: 'DataHub',
-                    link: 'https://macacajs.github.io/macaca-datahub/zh/'
-                  }
-                ]
-              },
-              {
-                text: '遍历工具',
-                items: [
-                  {
-                    text: 'NoSmoke',
-                    link: 'https://macacajs.github.io/NoSmoke/zh/'
-                  }
-                ]
-              }
-            ]
-          }
+          macacaEcosystem.zh,
         ],
         sidebar: {
           '/zh/guide/': genSidebarConfig(true),
